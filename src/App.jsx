b7d30c6054e2a59ef53be8d5c08df3e5c5227f35
mahdi ezati <mahdiezati80@gmail.com>
2024-09-05 23:45:47 +0330

    Added list of city and country

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,17 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of citie</p>} />
+          <Route path="cities" element={<p>list of citis</p>} />
+          <Route path="countries" element={<p> counries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,10 +1,17 @@
+import { NavLink } from "react-router-dom";
 import styles from "./AppNav.module.css";
 
 function AppNav() {
   return (
     <nav className={styles.nav}>
-      App navigation
-      <h1>app</h1>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
